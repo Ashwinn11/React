7bbf6ad64b54eaa3c1f788fc119d701decf2e927
Ashwinn11 <ashwinnanbazhagan@gmail.com>
2024-07-06 23:29:35 +0530

    use state integrate

--- reactPro/src/components/ListGroup.jsx
+++ reactPro/src/components/ListGroup.jsx
@@ -1,11 +1,18 @@
+import {useState} from 'react';
 const ListGroup = () => {
-  const items = ["New York", "chennai", "Dubai"];
+  const items = ["New York", "Chennai", "Dubai"];
+  const [selectedIndex,setSelectedIndex]=useState(-1)
+
   return (
     <>
-      <ul className="list-group">List</ul>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
+      <ul className="list-group">
+        <h2>List</h2>
+        {items.map((item,index) => (
+          <li className={ selectedIndex===index? 'list-group-item active' :'list-group-item'} key={item} onClick={()=>setSelectedIndex(index)} >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
